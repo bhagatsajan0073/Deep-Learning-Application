223ede673d1755414289ca3f8ee04349302b2eb9
Sajan Kumar <bhagat.sajan0073@gmail.com>
2019-06-13 15:04:34 +0530

    tensorflow version commit

--- Tensorflow Practice/tensorflow_check.py
+++ Tensorflow Practice/tensorflow_check.py
@@ -1,2 +1,6 @@
 import tensorflow
+import pandas as pd
+import numpy as np
 print("Tensorflow Version :",tensorflow.__version__)
+print("Pandas Version :",pd.__version__)
+print("Numpy Version :",np.__version__)
